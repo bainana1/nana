db41e92cc0eddeb6f4eebd9f49bbbc2b9cfaef2f
bnn <bnn@qq.com>
2019-10-11 10:59:03 +0800

    上传day5

---
+++ src/main/java/com/bw/entity/Demo.java
@@ -0,0 +1,11 @@
+package com.bw.entity;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("helloWord");
+	}
+
+	public void show(){
+		System.out.println("helloWord");
+	}
+}
---
+++ src/test/java/com/bw/test/test2.java
@@ -0,0 +1,19 @@
+package com.bw.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.bw.entity.Demo;
+
+public class test2 {
+
+	@Test
+	public void test() {
+		Demo demo = new Demo();
+		demo.show();
+		
+		
+	}
+
+}
